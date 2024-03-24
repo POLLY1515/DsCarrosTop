a80fbaf090e4370d4a39862c852d0f685717370c
POLLY1515 <amarantepolly2030@gmail.com>
2024-03-23 23:35:03 -0300

    Project created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+
+import './styles.css';
+
+export default function Header() {
+  return (
+
+    
+    <header>
+      <div className="ct-container">
+        <h1>Carros top</h1>
+      </div>
+    </header>
+    
+    
+    
+  );
+}
